48ea6785b79b19f9d20523ef42269db8ade4c8d5
Daniel Omine <danielomine@gmail.com>
2013-10-04 19:18:12 +0900

    - Including files from templates requires full path

--- app/Helpers/HTML/Layout/Bottom.php
+++ app/Helpers/HTML/Layout/Bottom.php
@@ -8,7 +8,7 @@
 * @license http://opensource.org/licenses/GPL-3.0 GNU Public License
 * @company: Tipui Co. Ltda.
 * @author: Daniel Omine <omine@tipui.com>
-* @updated: 2013-10-04 13:57:00
+* @updated: 2013-10-04 19:13:00
 */
 
 namespace Tipui\app\Helpers\HTML\Layout;
@@ -22,7 +22,9 @@ class Bottom
 	*/
 	public function Exec( $file = false )
 	{
-		include !$file ? DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '_html_bottom.html' :  $file;
+		//include ini_get( 'include_path' ) . ( !$file ? '..' . DIRECTORY_SEPARATOR . '_html_bottom.html' : $file );
+		include get_include_path() . ( !$file ? '..' . DIRECTORY_SEPARATOR . '_html_bottom.html' : $file );
+		//include ( !$file ? '.' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '_html_bottom.html' : $file );
 	}
 
 }
