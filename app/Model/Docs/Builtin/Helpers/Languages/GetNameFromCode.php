483c887cd5a743b237a54e61342a2e65dac827a0
Daniel Omine <danielomine@gmail.com>
2014-03-02 23:13:51 +0900

    - Added documentation for Builtin/Helper/Languages::GetNameFromCode

---
+++ app/Model/Docs/Builtin/Helpers/Languages/GetNameFromCode.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* @class  GetNameFromCode
+* @file   GetNameFromCode.php
+* @brief  GetNameFromCode Docs/Builtin/Helpers/Languages Module.
+* @date   2014-03-02 19:27:00
+* @license http://opensource.org/licenses/GPL-3.0 GNU Public License
+* @company: Tipui Co. Ltda.
+* @author: Daniel Omine <omine@tipui.com>
+* @updated: 2014-03-02 19:27:00
+*/
+
+namespace Tipui\App\Model\Docs\Builtin\Helpers\Languages;
+
+class GetNameFromCode
+{
+
+	/**
+	* @brief Data to be rendered
+	*/
+    public function View(){}
+
+}
